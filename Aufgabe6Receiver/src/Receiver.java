d6d82e475d3308223a6bddabb18b1b515c490777
unknown <Butterfly@.(none)>
2010-09-09 11:36:47 +0200

    comments added to Aufgabe6 Sender and Receiver

--- Aufgabe6Receiver/src/Receiver.java
+++ Aufgabe6Receiver/src/Receiver.java
@@ -5,49 +5,73 @@ import lejos.nxt.*;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
 
+/**
+ * Class that represents the Receiver of a communication between two bricks. The
+ * sender builds up the connection and sends information about how often its
+ * buttons have been pressed and also tells the receiver whether it will send
+ * more information or terminate now. The receiver than displays that
+ * information.
+ * 
+ * @author Wiebke Koepp
+ * @author Till Rohrmann
+ * 
+ */
 public class Receiver {
 
+	// counter for the three buttons: enter, left and right
 	private static int enter = 0;
 	private static int left = 0;
-	private static int right = 0;	
-	
-	public static void main(String [ ] args){
+	private static int right = 0;
+
+	public static void main(String[] args) {
+
+		// wait for the connection to be established
+		NXTConnection con = Bluetooth.waitForConnection();
 		
-		NXTConnection connectionBT = Bluetooth.waitForConnection();
-		DataInputStream disBT =
-			connectionBT.openDataInputStream();
-		
+		// open a data input stream to be able to read the data
+		DataInputStream dis = con.openDataInputStream();
+
+		// boolean that is false until sender wants to terminate
 		boolean terminate = false;
-		
-		while(!terminate){
+
+		// the first number of the information send by the sender is either a 0
+		// or a 1
+		// if it is a 1, the sender wants to send new numbers
+		// if it is a 0, the sender want to terminate
+		while (!terminate) {
 			try {
-				if(disBT.readInt() == 1){
-					enter = disBT.readInt();
-					left = disBT.readInt();
-					right = disBT.readInt();
+				// if the sender sends a 1, the next three numbers will be the
+				// counts for
+				// the buttons
+				if (dis.readInt() == 1) {
+					enter = dis.readInt();
+					left = dis.readInt();
+					right = dis.readInt();
 				}
-				else{
+				// sender sends a 0, there will be no more information
+				// -> make the while-loop terminate
+				else {
 					terminate = true;
 				}
 			} catch (IOException e) {
-				
+
 			}
+			// show the numbers on the display
 			LCD.drawString("Enter:" + enter, 0, 1);
 			LCD.drawString("Left:" + left, 0, 2);
 			LCD.drawString("Right:" + right, 0, 3);
 		}
-		
+
 		LCD.drawString("Terminated!", 0, 5);
+
+		// close connection and stream
 		try {
-			disBT.close();
+			dis.close();
 		} catch (IOException e) {
-		
+
 		}
-		
-		connectionBT.close();
-		
+		con.close();
+
 	}
 
-	
-	
 }
--- Aufgabe6Sender/src/Sender.java
+++ Aufgabe6Sender/src/Sender.java
@@ -8,62 +8,89 @@ import lejos.nxt.LCD;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
 
-
+/**
+ * Class that represents the Sender of a communication between two bricks. The
+ * sender builds up the connection and sends information about how often its
+ * buttons have been pressed and also tells the receiver whether it will send
+ * more information or terminate now. The receiver than displays that
+ * information.
+ * 
+ * @author Wiebke Koepp
+ * @author Till Rohrmann
+ * 
+ */
 public class Sender {
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
+
+		// bricks get to know each other (explorer = name of other brick)
 		RemoteDevice rd = Bluetooth.getKnownDevice("explorer");
-		
-		if(rd == null){
-			LCD.drawString("could not find",0,0);
-			LCD.drawString("explorer",0,1);
+
+		// error handling -> device could not be found
+		if (rd == null) {
+			LCD.drawString("Could not find", 0, 0);
+			LCD.drawString("explorer.", 0, 1);
 			Button.waitForPress();
 			System.exit(1);
 		}
+
+		// open the connection to the other brick
 		NXTConnection con = Bluetooth.connect(rd);
-		
-		if(con == null){
+
+		// error handling -> connecting did not work
+		if (con == null) {
 			LCD.clear();
-			LCD.drawString("could not establish", 0, 0);
-			LCD.drawString("connection",0,1);
+			LCD.drawString("Could not establish", 0, 0);
+			LCD.drawString("connection.", 0, 1);
 			Button.waitForPress();
 			System.exit(1);
 		}
-		
+
 		LCD.clear();
-		LCD.drawString("con established", 0,0);
-		
+		LCD.drawString("Con established.", 0, 0);
+
+		// open data out put stream to send information
 		DataOutputStream dos = con.openDataOutputStream();
-		
-		int left =0;
+
+		// initialize counters for the different buttons and a boolean for
+		// termination
+		int left = 0;
 		int right = 0;
 		int enter = 0;
-		boolean escapePressed=false;
-		
-		while(!escapePressed){
+		boolean escapePressed = false;
+
+		while (!escapePressed) {
+			// wait for press returns value with bits set or not set for every
+			// button
 			int id = Button.waitForPress();
-			if((id&1)!=0){
+			// 0x01 -> Enter
+			if ((id & 1) != 0) {
 				LCD.clear();
-				LCD.drawString("enter pressed",0,0);
+				LCD.drawString("Enter pressed.", 0, 0);
 				enter++;
 			}
-			if((id&2)!=0){
+			// 0x02 -> Left
+			if ((id & 2) != 0) {
 				LCD.clear();
-				LCD.drawString("left pressed",0,1);
+				LCD.drawString("Left pressed.", 0, 1);
 				left++;
 			}
-			if((id&4)!=0){
+			// 0x04 -> Right
+			if ((id & 4) != 0) {
 				LCD.clear();
-				LCD.drawString("right pressed",0,2);
+				LCD.drawString("Right pressed.", 0, 2);
 				right++;
 			}
-			if((id&8)!=0){
+			// 0x08 -> Escape
+			if ((id & 8) != 0) {
 				LCD.clear();
-				LCD.drawString("esc pressed",0,3);
+				LCD.drawString("Esc pressed.", 0, 3);
 				escapePressed = true;
 			}
-			
-			if(!escapePressed){
+
+			// Sender does not want to terminate -> Send a 1 followed by the
+			// counters for enter, left and right
+			if (!escapePressed) {
 				try {
 					dos.writeInt(1);
 					dos.writeInt(enter);
@@ -71,39 +98,41 @@ public class Sender {
 					dos.writeInt(right);
 				} catch (IOException e) {
 					LCD.drawString("error while", 0, 0);
-					LCD.drawString("sending data",0,1);
+					LCD.drawString("sending data", 0, 1);
 					System.exit(1);
 				}
 			}
-			else{
-				try{
+			// Sender wants to terminate -> Send a 0 and do all the necessary
+			// steps to terminate connection
+			else {
+				try {
 					dos.writeInt(0);
-				}catch(IOException e){
+				} catch (IOException e) {
 					LCD.drawString("error while", 0, 0);
-					LCD.drawString("sending term",0,1);
+					LCD.drawString("sending term", 0, 1);
 					System.exit(1);
 				}
 			}
-			
+
 			try {
 				dos.flush();
 			} catch (IOException e) {
 				LCD.drawString("error while", 0, 0);
-				LCD.drawString("flushing",0,1);
+				LCD.drawString("flushing", 0, 1);
 				System.exit(1);
 			}
 		}
-		
+
 		try {
 			dos.close();
 		} catch (IOException e) {
 			LCD.drawString("error while closing", 0, 0);
-			LCD.drawString("dos",0,1);
+			LCD.drawString("dos", 0, 1);
 			System.exit(1);
 		}
-		
+
 		con.close();
-		
+
 	}
 
 }
